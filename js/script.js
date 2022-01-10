f50f8d72daeccc2a8c8a895c610b880d487ea97e
Samuel Scozzari <s_scozzari@libero.it>
2022-01-10 17:17:38 +0100

    bonus completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function createMember() {
+  const lastMember = team.pop();
+  const teamCard = `
+      <div class="team-card">
+        <div class="card-image">
+          <img src="${lastMember.image}" />
+        </div>
+        <div class="card-text">
+          <h3>${lastMember.name}</h3>
+          <p>${lastMember.role}</p>
+        </div>
+      </div>
+      `;
+
+  document.querySelector(".team-container").innerHTML += teamCard;
+}
+
 const team = [
   {
     name: "Wayne Barnett",
@@ -33,16 +50,37 @@ const team = [
 
 for (let i = 1; i < team.length; i++) {
   const teamCard = `
-  <div class="team-card">
-    <div class="card-image">
-      <img src="img/${team[i].image}" />
+    <div class="team-card">
+      <div class="card-image">
+        <img src="img/${team[i].image}" />
+      </div>
+      <div class="card-text">
+        <h3>${team[i].name}</h3>
+        <p>${team[i].role}</p>
+      </div>
     </div>
-    <div class="card-text">
-      <h3>${team[i].name}</h3>
-      <p>${team[i].role}</p>
-    </div>
-  </div>
-`;
+    `;
 
   document.querySelector(".team-container").innerHTML += teamCard;
 }
+
+/* -------------------------------------------------------------------- */
+
+/* Bonus */
+/* Aggiungere immagini da internet */
+let addMember = document.getElementById("addMemberButton");
+addMember.addEventListener("click", function () {
+  let valName = document.getElementById("name").value;
+  let valRole = document.getElementById("role").value;
+  let valImage = document.getElementById("image").value;
+  team.push({
+    name: valName,
+    role: valRole,
+    image: valImage,
+  });
+  createMember();
+
+  document.getElementById("name").value = "";
+  document.getElementById("role").value = "";
+  document.getElementById("image").value = "";
+});
